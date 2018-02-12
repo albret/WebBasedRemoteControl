982142dc9467e49e4b197acac40bf9f87c7d42bb
EC2 Default User <ec2-user@ip-172-31-22-92.us-west-2.compute.internal>
2018-02-12 22:39:52 +0000

    Rewrite using expressjs this time, check point before adding render stuff

--- app.js
+++ app.js
@@ -1,65 +1,42 @@
-const http = require('http');
-const https = require('https');
-const fs = require('fs');
-const path = require('path');
-const baseDirectory = __dirname
+const express = require('express'),
+	fs = require('fs'),
+	http = express(),
+	https = require('https'),
+	app = express();
 
-// redirect all http request to https
-http.createServer((req, res) => {
-	var ip = req.headers['x-forwarded-for'] || 
-		req.connection.remoteAddress || 
-		req.socket.remoteAddress ||
-		(req.connection.socket ? req.connection.socket.remoteAddress : null);
-	console.log(ip + ': http redirect');
+var trafficLog = function(req, res, next) {
+	var ip = req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        (req.connection.socket ? req.connection.socket.remoteAddress : null);
+    console.log(ip + ':\t' + req.url);
+	next();
+}
+
+var redirectHttps = function(req, res, next) {
+	console.log('\t\t redirect');
 	if (req.url == '/')
-		redirectTo(req, res, '/index.html');
-	else
-		redirectTo(req, res, req.url);
-}).listen(8000);
+		req.url = '/index.html';
+	res.writeHead(302, {Location: 'https://' + req.headers.host + req.url});
+	res.end();
+}
 
-const options = {
+var default404 = function(req, res, next) {
+	res.writeHead(302, {Location: 'https://' + req.headers.host + '/404.html'});
+	res.end();
+}
+
+http.listen(8000);
+http.use(trafficLog);
+http.use(redirectHttps);
+
+
+const httpsOption = {
 	key: fs.readFileSync('/home/ec2-user/ssl-keys/privkey.pem'),
-	cert: fs.readFileSync('/home/ec2-user/ssl-keys/cert.pem'),
+    cert: fs.readFileSync('/home/ec2-user/ssl-keys/cert.pem'),
 };
+https.createServer(httpsOption, app).listen(8001);
 
-https.createServer(options, (req, res) => {
-	var ip = req.headers['x-forwarded-for'] || 
-		req.connection.remoteAddress || 
-		req.socket.remoteAddress ||
-		(req.connection.socket ? req.connection.socket.remoteAddress : null);
-	console.log(ip  + ':\t' + req.url);
-	try {
-		var pagePath = path.normalize(req.url);
-		var fullPath = baseDirectory + '/var' + pagePath;
-		if (pagePath == '/') {
-			redirectTo(req, res, '/index.html');
-		} else if (pagePath.slice(0, 3) == '/r/') {
-			apiHandle(req, res);
-		} else {
-			var fileStream = fs.createReadStream(fullPath);
-			fileStream.pipe(res);
-			fileStream.on('open', function() {
-				res.writeHead(200, {'Content-Type': 'text/html'});
-			});
-			fileStream.on('error', function(e) {
-				redirectTo(req, res, '/404.html');
-			})
-		}
-	} catch(e) {
-		res.writeHead(500);
-		res.end();
-		console.log(e.stack);
-	}
-}).listen(8001);
-
-function redirectTo(req, res, url) {
-	console.log('\t\tredirect');
-	res.writeHead(302, {Location: 'https://' + req.headers.host + url});
-	res.end();
-}
-
-// all api requests should start with /r/
-function apiHandle(req, res) {
-	console.log('api');
-	res.end();
-}
+app.use(trafficLog);
+app.use(express.static(__dirname + '/var'));
+app.use(default404);
