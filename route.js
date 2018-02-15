c1917223b2cca0dbab2aa0baab750663dd153a0b
EC2 Default User <ec2-user@ip-172-31-22-92.us-west-2.compute.internal>
2018-02-15 02:59:54 +0000

    Added ejs template files, logo, and style. Added route to views.

--- route.js
+++ route.js
@@ -5,9 +5,27 @@ module.exports = function(app) {
 	app.get('/test', function(req, res) {
     	res.render('index', {message: 'Hello World        --Kevin\n'});
 	});
+
+	app.get('/', function(req, res) {
+		res.render('RClogin');
+	});
 	
 	app.get('/account/:user', function(req, res) {
 		var username = req.params.user;
 		//TODO
 	});
+
+	app.get('/logo', function(req,res) {
+		res.sendFile('/home/ec2-user/workspace/WebBasedRemoteControl/views/logo.png');
+	});
+	
+	app.get('/style.css', function(req,res) {
+    	res.sendFile('/home/ec2-user/workspace/WebBasedRemoteControl/views/style.css');
+    });
+
+	app.get('/createAccount', function(req, res) {
+		res.render('AccountCreate');
+	});
+
+
 };
