399233f266102b46aeec79658dc7bbd831b216b8
EC2 Default User <ec2-user@ip-172-31-22-92.us-west-2.compute.internal>
2018-02-21 19:32:49 +0000

    added comments to webclient

--- webclient.py
+++ webclient.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python
 
+#Please download this to your local computer. This script will not work on the server.
 from websocket import create_connection
+#connect to server and specify random-number protocol to get random numbers
 ws = create_connection("wss://applepie.albert.tech", subprotocols=["random-number"])
 print "Requesting numbers..."
+#Send "Give me numbers" to the server
 ws.send("Give me numbers")
 print "Requested"
 print "Receiving..."
+#While loop to continually wait for server to return messages
 while 1:
     result =  ws.recv()
+    #Messages from server will be stored in 'result'. Currently the server sends 25 random numbers between 0 and 99 and then stops.
     print "Received '%s'" % result
 ws.close()
