898b4629cb33a73bdebd45ef163b5a4a75e4d9b3
EC2 Default User <ec2-user@ip-172-31-22-92.us-west-2.compute.internal>
2018-02-21 02:36:41 +0000

    test websocket

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const express = require('express'),
           cert: fs.readFileSync('/home/ec2-user/ssl-keys/cert.pem'),
       },
       app = express(),
-      cookieParser = require('cookie-parser');
+      cookieParser = require('cookie-parser'),
+      WebSocketServer = require('websocket').server;
+
 // Log ip and url of all traffic
 var trafficLog = function(req, res, next) {
     var ip = req.headers['x-forwarded-for'] ||
@@ -37,12 +39,44 @@ http.use(trafficLog);
 http.use(redirectHttps);
 
 // Start server with options
-https.createServer(httpsOption, app).listen(8001);
+var secureServer = https.createServer(httpsOption, app).listen(8001);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser(require('./config').conf.cookieconfig.signkey));
+
+// Websocket server startup
+wsServer = new WebSocketServer({
+    httpServer: secureServer,
+    autoAcceptConnections: false
+});
+
+function originIsAllowed(origin) {
+    //TODO add logic to determine if correct origin
+    console.log(origin + ' attempting to connect.');
+    return true;
+}
+
+//Websocket receive connection requests
+wsServer.on('request', function(request) {
+    if (!originIsAllowed(request.origin)) {
+      request.reject();
+      console.log('Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
+    var connection = request.accept('echo-protocol', request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            connection.sendUTF(message.utf8Data);
+        }
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + connection.remoteAddress + ' disconnected.');
+    });
+})
 
 // Express middleware
 app.use(trafficLog);
